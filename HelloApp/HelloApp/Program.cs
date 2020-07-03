4d6563e7081bb0c65b57278e7cdd92830053e2ef
Moon-Kyeongmin <rudals9204@naver.com>
2020-07-03 14:25:46 +0900

    test

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static System.Console;
 
+/// <summary>
+/// HelloApp namespace
+/// </summary>
 namespace HelloApp
 {
+    /// <summary>
+    /// 기본 클래스
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            if (args.Length == 0)
+            {
+                WriteLine("HelloApp.exe name");
+                return;
+            }
+
+            WriteLine($"Hello,{args[0]}");
         }
     }
 }
